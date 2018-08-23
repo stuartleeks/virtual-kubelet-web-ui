1a7f4f92950e5bba507147364adb4217c612d158
Stuart Leeks <stuartle@microsoft.com>
2018-08-23 20:25:39 +0100

    Add more Pod/Container info

--- src/PodDetail.js
+++ src/PodDetail.js
@@ -1,5 +1,72 @@
 import React, { Component } from 'react';
 
+const PodSummary = props => {
+    const { pod, killPod } = props;
+    return (
+        <div>
+            <div className="pod-summary">
+                <div>
+                    <span className="title" >namespace</span>
+                    <span className="value" >{pod.metadata.namespace}</span>
+                </div>
+                <div>
+                    <span className="title" >podname</span>
+                    <span className="value" >{pod.metadata.name}</span>
+                </div>
+                <div>
+                    <span className="title" >phase</span>
+                    <span className="value" >{pod.status.phase}</span>
+                </div>
+                <button onClick={() => killPod(pod)}>Stop pod</button>
+            </div>
+            <div className="pod-container-list">
+                <h3>Containers</h3>
+                <ContainerList pod={pod} />
+            </div>
+        </div>
+    )
+}
+const ContainerList = props => {
+    const { pod } = props;
+    const containers = pod.spec.containers.map((container, index) => {
+        const containerStatuses = pod.status.containerStatuses.filter((c, i) => c.name === container.name);
+        const containerStatus = containerStatuses[0];
+        return (
+            <div key={index}>
+                <h4>Container: {container.name} ({container.image})</h4>
+                <div>
+                    <span className="title" >image</span>
+                    <span className="value" >{container.image}</span>
+                </div>
+                <div>
+                    <span className="title" >ports</span>
+                    <span className="value" ><ContainerPortList container={container} /></span>
+                </div>
+                <div>
+                    <span className="title" >ready</span>
+                    <span className="value" >{containerStatus.ready ? "true" : "false"}</span>
+                </div>
+                <div>
+                    <span className="title" >restart count</span>
+                    <span className="value" >{containerStatus.restartCount}</span>
+                </div>
+                <div>
+                    <span className="title" >state</span>
+                    <span className="value" >{JSON.stringify(containerStatus.state)}</span>
+                </div>
+            </div>
+        )
+    });
+    return <div>{containers}</div>
+}
+const ContainerPortList = props => {
+    const { container } = props;
+    const ports = container.ports.map((port, index) => {
+        let separator = index > 0 ? ", " : ""
+        return <span key={index}>{separator}{port.name} ({port.containerPort}/{port.protocol})</span>
+    });
+    return (<span>{ports}</span>)
+}
 class PodDetail extends Component {
     render() {
         const { pod, killPod } = this.props;
@@ -8,26 +75,14 @@ class PodDetail extends Component {
         if (pod !== null) {
             gotPod = (
                 <div>
-                    <div>
-                        <span className="title" >namespace</span>
-                        <span className="value" >{pod.metadata.namespace}</span>
-                    </div>
-                    <div>
-                        <span className="title" >podname</span>
-                        <span className="value" >{pod.metadata.name}</span>
-                    </div>
-                    <div>
-                        <span className="title" >phase</span>
-                        <span className="value" >{pod.status.phase}</span>
-                    </div>
-                    <button onClick={()=>killPod(pod)}>Stop pod</button>
+                    <PodSummary pod={pod} killPod={killPod} />
                 </div>
             );
         }
 
         let content = pod === null ? noPod : gotPod;
         return (
-            <div className="selectedPod">{content}</div>
+            <div className="selected-pod">{content}</div>
         );
     }
 
